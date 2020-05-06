725c0005d015c5e35685317c2381eccd14d6848b
Austin Aspaas <araspaas@live.com>
2020-05-05 20:55:50 -0600

    added test rank of card to deck_test.rb

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -10,11 +10,15 @@ class DeckTest < Minitest::Test
     card2 = Card.new(:spade, "3", 3)
     card3 = Card.new(:heart, "Ace", 14)
     cards = [card1, card2, card3]
-    deck = Deck.new(cards)
-    binding.pry
+    @deck = Deck.new(cards)
   end
 
   def test_it_exists
-    deck = Deck.new()
+    assert_instance_of Deck, @deck
+  end
+
+  def test_rank_of_card_at
+    assert_equal 12, @deck.rank_of_card_at(0)
+    p "Rank of card at 0: " + @deck.rank_of_card_at(0).to_s
   end
 end
