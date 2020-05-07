9f3407b6139345288a0775087b9ec82dad0ef463
Austin Aspaas <araspaas@live.com>
2020-05-06 20:46:55 -0600

    created test high ranking cards

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -6,10 +6,10 @@ require "pry"
 
 class DeckTest < Minitest::Test
   def setup
-    card1 = Card.new(:diamond, "queen", 12)
-    card2 = Card.new(:spade, "3", 3)
-    card3 = Card.new(:heart, "Ace", 14)
-    cards = [card1, card2, card3]
+    @card1 = Card.new(:diamond, "queen", 12)
+    @card2 = Card.new(:spade, "3", 3)
+    @card3 = Card.new(:heart, "Ace", 14)
+    cards = [@card1, @card2, @card3]
     @deck = Deck.new(cards)
   end
 
@@ -20,5 +20,9 @@ class DeckTest < Minitest::Test
   def test_rank_of_card_at
     assert_equal 12, @deck.rank_of_card_at(0)
     p "Rank of card at 0: " + @deck.rank_of_card_at(0).to_s
+  end
+
+  def test_high_ranking_cards
+    assert_equal [@card1, @card3], @deck.high_ranking_cards
   end
 end
