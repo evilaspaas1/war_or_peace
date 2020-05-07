19dd1965ebf851db48eeb14d0abac1151953d503
Austin Aspaas <araspaas@live.com>
2020-05-06 20:44:49 -0600

    added high ranking cards method

--- lib/deck.rb
+++ lib/deck.rb
@@ -7,4 +7,14 @@ class Deck
   def rank_of_card_at(number)
     @cards[number].rank
   end
+
+  def high_ranking_cards
+    high_ranked_cards = []
+    @cards.each do |card|
+      if card.rank >= 11
+        high_ranked_cards << card
+      end
+    end
+    high_ranked_cards
+  end
 end
