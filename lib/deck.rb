a1465054db8671e9d0df304a2ad7ce3f2831c5a1
Austin Aspaas <araspaas@live.com>
2020-06-30 19:37:28 -0600

    Add percent high ranking method and pass test. fix typos in high ranking cards for local variable

--- lib/deck.rb
+++ lib/deck.rb
@@ -10,12 +10,18 @@ class Deck
   end
 
   def high_ranking_cards
-    high_ranking_cards = []
+    high_ranked_cards = []
     @cards.each do |card|
       if card.rank >= 11
-        high_ranking_cards << card
+        high_ranked_cards << card
       end
     end
-    high_ranking_cards 
+    high_ranked_cards
+  end
+
+  def percent_high_ranking
+    percentage = high_ranking_cards.length.to_f / @cards.length.to_f
+    multiplied = percentage * 100
+    multiplied.round(2)
   end
 end
