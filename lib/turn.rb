67a91ec23150e1d001c97bf40a5d14ac1550e7dd
Austin Aspaas <araspaas@live.com>
2020-07-03 10:51:41 -0600

    Add :war type to type method and pass test

--- lib/turn.rb
+++ lib/turn.rb
@@ -10,7 +10,9 @@ class Turn
   end
 
   def type
-    if @player1.deck.cards[0].rank != @player2.deck.cards[0].rank
+    if @player1.deck.cards[0].rank == @player2.deck.cards[0].rank
+      :war
+    elsif @player1.deck.cards[0].rank != @player2.deck.cards[0].rank
       :basic
     end
   end
